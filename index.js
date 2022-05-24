237779fcb2cae7a21fd3292f27b9595d9136845a
Cristiano Sales <cristianosalesneves@gmail.com>
2022-05-24 16:50:21 -0300

    1 - Crie o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
-const app = express();
+const app = express(); // Criada uma nova aplicação Express
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -10,8 +11,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+  //  console.log(response);
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+// 1 - Crie o endpoint GET /talker
+app.get('/talker', (_request, response) => { // Dizer ao Express que, ao tratar uma requisição com método GET no caminho /talker, envie o status HTTP 200, que significa OK, e o json dado
+  const api = JSON.parse(fs.readFileSync('./talker.json'));
+  response.status(HTTP_OK_STATUS).json(api);
 });
+
+app.listen(PORT, () => { // Pedir ao Express que crie um servidor HTTP e escute por requisições na porta 3000
+  console.log('Online');
+}); 
