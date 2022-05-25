5d582b407ad776e1a771464a477bfd0f4058e224
Cristiano Sales <cristianosalesneves@gmail.com>
2022-05-24 23:52:58 -0300

    6 - Crie o endpoint PUT /talker/:id

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { token: tokenGenerator } = require('./token');
 const middlewares = require('./middlewares');
+
+const talkerJsonPath = './talker.json';
 const { HTTP_OK_STATUS, NOT_FOUND_STATUS, CREATED_STATUS } = require('./utils/status');
 
 const app = express(); // Criada uma nova aplicação Express
@@ -17,7 +19,7 @@ app.get('/', (_request, response) => {
 
 // 1 - Crie o endpoint GET /talker
 app.get('/talker', (_request, response) => { // Dizer ao Express que, ao tratar uma requisição com método GET no caminho /talker, envie o status HTTP 200, que significa OK, e o json dado
-  const api = JSON.parse(fs.readFileSync('./talker.json'));
+  const api = JSON.parse(fs.readFileSync(talkerJsonPath));
   response
     .status(HTTP_OK_STATUS)
     .json(api);
@@ -26,7 +28,7 @@ app.get('/talker', (_request, response) => { // Dizer ao Express que, ao tratar 
 // 2 - Crie o endpoint GET /talker/:id
 app.get('/talker/:id', (request, response) => {
   const { id: identifier } = request.params;
-  const idApi = JSON.parse(fs.readFileSync('./talker.json'))
+  const idApi = JSON.parse(fs.readFileSync(talkerJsonPath))
     .find(({ id }) => Number(id) === Number(identifier));
   if (idApi) return response.status(HTTP_OK_STATUS).json(idApi);
   response
@@ -51,14 +53,33 @@ app.post('/talker',
   middlewares.talkField,
   (request, response) => {
     const { name, age, talk } = request.body;
-    const api = JSON.parse(fs.readFileSync('./talker.json'));
+    const api = JSON.parse(fs.readFileSync(talkerJsonPath));
     const lastId = api[api.length - 1];
     const id = lastId === undefined ? 0 : lastId.id + 1;
     const newOBJ = { id, name, age, talk };
     api.push(newOBJ);
-    fs.writeFileSync('./talker.json', JSON.stringify(api), 'utf-8');
+    fs.writeFileSync(talkerJsonPath, JSON.stringify(api), 'utf-8');
     response.status(CREATED_STATUS).json(newOBJ);
   });
+
+app.put('/talker/:id',
+  middlewares.authorization,
+  middlewares.talkerRegister,
+  middlewares.talkFieldIsRequired,
+  middlewares.talkField,
+  (request, response) => {
+    const { name, age, talk } = request.body;
+    const { id } = request.params;
+    const api = JSON.parse(fs.readFileSync(talkerJsonPath));
+    const newOBJ = { id: Number(id), name, age, talk };
+    const apiPUT = api.map((obj) => {
+  if (Number(obj.id) === Number(id)) return newOBJ;
+  return obj;
+ });
+ 
+  fs.writeFileSync(talkerJsonPath, JSON.stringify(apiPUT), 'utf-8');
+  response.status(HTTP_OK_STATUS).json(newOBJ);
+ });
 
 // app.use(middlewares.errorHandler);
 
